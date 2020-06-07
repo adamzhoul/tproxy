3beae607bf9010da52ae9f2d1aec25f5da294206
adamzhoul <ljylei@163.com>
2020-06-07 15:37:15 +0000

    update

--- pkg/proxy/proxy_test.go
+++ pkg/proxy/proxy_test.go
@@ -1,7 +1,28 @@
 package proxy
 
-import "testing"
+import (
+	"log"
+	"net/http"
+	"net/url"
+	"testing"
+)
 
 func TestProxy(t *testing.T) {
+
+	proxy, _ := url.Parse("http://127.0.0.1:9090")
+	tr := &http.Transport{
+		Proxy: http.ProxyURL(proxy),
+	}
+
+	req, _ := http.NewRequest("GET", "http://www.zl.com", nil)
+	client := &http.Client{}
+	client.Transport = tr
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	log.Println(res.StatusCode, req.Response)
 
 }
